47762d7198b948be7354c5e4ce69aebbb3f0df1a
Victoria Yaakov <victoriaja1916@gmail.com>
2024-05-26 11:51:46 +0300

    added ecr and push image to the ecr

---
+++ ecr.tf
@@ -0,0 +1,25 @@
+resource "docker_image" "redis_python_image" {
+  name = "redis_python"
+  build {
+    path = "."
+    dockerfile = "Dockerfile"
+    }
+  }
+
+
+# Start a container
+resource "docker_container" "redis_python_container" {
+  name  = "redis_python_container"
+  image = docker_image.redis_python_image.latest
+}
+
+
+# Create a ECR repository
+resource "aws_ecr_repository" "redis_python_service" {
+  name                 = "redis_python-service"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
